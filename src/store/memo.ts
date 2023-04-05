7f52fb0c94eb859ea220e0a00001290985ff9abf
dan <50140505+steely-d@users.noreply.github.com>
2023-04-06 00:07:58 +0900

    add memo store

---
+++ src/store/memo.ts
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+
+export type MemoState = { memoList: Memo[] };
+
+const initialState: MemoState = { memoList: [] };
+
+export const useMemoStore = defineStore("memo", {
+  state: () => initialState,
+  actions: {
+    addMemo(payload: Memo) {
+      this.memoList = [...this.memoList, payload];
+    },
+    updateMemoById(id: string, partial: Partial<Memo>) {
+      this.memoList = this.memoList.map((memo) => {
+        if (memo.id === id) {
+          return { ...memo, ...partial };
+        }
+        return memo;
+      });
+    },
+    deleteMemoById(id: string) {
+      this.memoList = this.memoList.filter((memo) => memo.id !== id);
+    },
+  },
+});
